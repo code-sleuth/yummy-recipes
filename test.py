3f7f0a23f719dfff5354d89620f0ad9c8e2b37ab
Ibrahim Mbaziira <code.ibra@gmail.com>
2017-09-26 10:46:22 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+class Test(unittest.TestCase)
+    def setup(self):
+        print("sometext")
+        
+    def test_search_in_python_org(self):
+        print("sometext")
+    
+    def teardown(self):
+        print("sometext")
+    
+if __name__ == "__main__":
+    unittest.main()
+        
