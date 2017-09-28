b742324634c134c16485760d9cf37e0cf655ea28
ibra_code <code.ibra@gmail.com>
2017-09-28 16:05:52 +0300

    Updated Unit Tests, Fixed some Crud, Added UML Diagram

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 [![Build Status](https://travis-ci.org/code-sleuth/yummy-recipes.svg?branch=dev)](https://travis-ci.org/code-sleuth/yummy-recipes)
 [![Coverage Status](https://coveralls.io/repos/github/code-sleuth/yummy-recipes/badge.svg?branch=dev)](https://coveralls.io/github/code-sleuth/yummy-recipes?branch=dev)
+[![Code Climate](https://codeclimate.com/github/cloudfoundry/membrane.png)](https://codeclimate.com/github/code-sleuth/yummy-recipes)
 # Yummy Recipes
 ## Description
 #### The innovative yummy recipes app is an application that allows users to create, save and share meeting the needs of keeping track of awesome food recipes
@@ -7,14 +8,56 @@
 - Flask 0.12.2
 - Python 3.6 - 3.6.2
 - Bootstrap 3.2*
+- jQeury 3.2.*
 - HTML 5
 - Google chrome is the preferred browser
+- Screen size ***all***
 ### Project is hosted on
-[yummy recipes on GitHub pages](https://code-sleuth.github.io)
-[yummy recipes on Heroku](https://morning-escarpment-76742.herokuapp.com/)
+- [yummy recipes on GitHub pages](https://code-sleuth.github.io)
+- [yummy recipes on Heroku](https://morning-escarpment-76742.herokuapp.com/)
 
 #### How to set up project
-- clone or download project from my github repository [yummy recipes on GitHub pages](https://github.com/code-sleuth/yummy-recipes)
-- set up vitual environment
+- clone or download project from my github repository [yummy recipes on GitHub pages](https://github.com/code-sleuth/code-sleuth.github.io)
+- set up virtual environment
 - set up flask
 - run app.py
+
+* [how to set up flask](http://flask.pocoo.org/docs/0.12/installation/)
+* [how to set up virtual environment](http://docs.python-guide.org/en/latest/dev/virtualenvs/)
+* then follow the steps below
+##### More on environment/dependencies set up
+
+* set up git and clone/download the repository
+    ```
+        $ https://github.com/code-sleuth/yummy-recipes
+    ```
+
+* ###### Install  requirements.txt contents
+    ```
+    (venv)$ pip install -r requirements.txt
+    ```
+
+* #### Running It
+    On your terminal, run the server using this one simple command:
+    ```
+    (venv)$ flask app.py
+    ```
+* ##### Run Tests from unit tests file
+    On your terminal, run the server using this one simple command:
+    ```
+    (venv)$ python tests/unit_tests.py
+    ```
+* ##### Heroku
+    ```
+    https://morning-escarpment-76742.herokuapp.com
+    ```
+ ## More on testing, account for quick access
+
+* Username: admin
+* Password: pass
+
+ * #### Team
+    * ****decepticonSociety****
+    
+ * ##### Author
+    * **Ibrahim Mbaziira**
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,5 +1,5 @@
 import unittest
-
+from app import app
 from models import User, Category, Recipe
 
 
@@ -14,16 +14,32 @@ class MinimalTestUser(unittest.TestCase):
         self._user.increment_users_list("ibm", "ibra stacks", "decepticon@gmail.com", "123")
         self.assertEqual(self._user.check_user_login("ibm", "123"), "User credentials ok")
 
+    def test_check_user_login_failed(self):
+        self._user.increment_users_list("ibm", "ibra stacks", "decepticon@gmail.com", "123")
+        self.assertEqual(self._user.check_user_login("x", "y"), "Wrong username or password", msg="user not in record")
+
     def test_add_new_user(self):
         self.assertEqual(self._user.add_new_user("dan", "Daniel Sunku", "danny@gmail.com", "dannyboy"), "User Added")
 
+    def test_add_user_failed(self):
+        self._user.add_new_user("dan", "Daniel Sunku", "danny@gmail.com", "dannyboy")
+        self.assertEqual(self._user.add_new_user("dan", "Daniel Sunku", "danny@gmail.com", "dannyboy"), "User exists")
+
     def test_edit_user(self):
         self._user.increment_users_list("dan", "Daniel Sunku", "danny@gmail.com", "dannyboy")
-        self.assertEqual(self._user.edit_user("dan", "Daniel Sun", "dan@gmail.com", "dboy"), "Updated user successfully")
+        self.assertEqual(self._user.edit_user("dan", "Daniel Sun", "dan@gmail.com", "dboy"), "Updated user details")
+
+    def test_edit_user_failed(self):
+        self._user.increment_users_list("dan", "Daniel Sunku", "danny@gmail.com", "dannyboy")
+        self.assertEqual(self._user.edit_user("da", "Daniel Sun", "dan@gmail.com", "dboy"), "Failed to update user")
 
     def test_delete_user(self):
         self._user.increment_users_list("dan", "Daniel Sunku", "danny@gmail.com", "dannyboy")
         self.assertEqual(self._user.delete_user("dan"), "User deleted successfully")
+
+    def test_delete_user_failed(self):
+        self._user.increment_users_list("dan", "Daniel Sunku", "danny@gmail.com", "dannyboy")
+        self.assertEqual(self._user.delete_user("da"), "Failed to delete user", msg="User doesn't exist")
 
 
 class MinimalTestCategory(unittest.TestCase):
@@ -36,17 +52,33 @@ class MinimalTestCategory(unittest.TestCase):
     def test_add_category(self):
         self.assertEqual(self._category.add_category("Pineapple Punch"), "category added successfully")
 
+    def test_add_category_failed(self):
+        self._category.increment_categories_list("Foodie Category")
+        self.assertEqual(self._category.add_category("Foodie Category"), "category  exists")
+
     def test_edit_category(self):
         self._category.increment_categories_list("Pineapple Punch")
-        self.assertEqual(self._category.edit_category("apple Punch", "Pineapple Punch"), "Category updated successfully")
+        self.assertEqual(self._category.edit_category("apple Punch", "Pineapple Punch"), "Category updated")
+
+    def test_edit_category_failed(self):
+        self._category.increment_categories_list("Pineapple Punch")
+        self.assertEqual(self._category.edit_category("apple Punch", "Pine Punch"), "category doesn't exist")
 
     def test_delete_category(self):
         self._category.increment_categories_list("Fruits Salad")
         self.assertEqual(self._category.delete_category("Fruits Salad"), "category deleted")
 
+    def test_delete_category_failed(self):
+        self._category.increment_categories_list("Fruits Salad")
+        self.assertEqual(self._category.delete_category("Mango"), "category does not exist", msg="Category not found")
+
     def test_get_category(self):
         self._category.increment_categories_list("My Category")
         self.assertEqual(self._category.get_category("My Category"), "My Category")
+
+    def test_get_category_failed(self):
+        self._category.increment_categories_list("Another One")
+        self.assertEqual(self._category.get_category("XO"), "category does not exist", msg="Category not found")
 
 
 class MinimalTestRecipe(unittest.TestCase):
@@ -60,13 +92,45 @@ class MinimalTestRecipe(unittest.TestCase):
         self._recipe.increment_recipe_list("Fries", "Potatos", "Smashed fries", "Oil")
         self.assertEqual(self._recipe.add_recipe("Carrot spice", "spices", "minced", "sun dry"), "Recipe added")
 
+    def test_add_recipe_failed(self):
+        self._recipe.increment_recipe_list("Fries", "Potatos", "Smashed fries", "Oil")
+        self.assertEqual(self._recipe.add_recipe("Fries", "Potatos", "Smashed fries", "Oil"), "recipe already exists")
+
     def test_edit_recipe(self):
         self._recipe.increment_recipe_list("Fries", "Potatos", "Smashed fries", "Oil")
         self.assertEqual(self._recipe.edit_recipe("Fries", "Potat", "Smashed All", "Olive oil"), "recipe updated")
 
+    def test_edit_recipe_failed(self):
+        self._recipe.increment_recipe_list("Fries", "Potatos", "Smashed fries", "Oil")
+        self.assertEqual(self._recipe.edit_recipe("Fry", "Potato", "Smashed All", "Olive oil"), "Failed recipe update")
+
     def test_delete_recipe(self):
         self._recipe.increment_recipe_list("Fries", "Potatos", "Smashed fries", "Oil")
         self.assertEqual(self._recipe.delete_recipe("Fries"), "recipe deleted successfully")
+
+    def test_delete_recipe_failed(self):
+        self._recipe.increment_recipe_list("Fries", "Potatos", "Smashed fries", "Oil")
+        self.assertEqual(self._recipe.delete_recipe("none"), "failed to delete recipe", msg="Recipe doesn't exist")
+
+
+class MinimalTestFlask(unittest.TestCase):
+    # test login page, index page and dashboard
+    def get_ping(self):
+        ping = app.test_client(self)
+        return ping
+
+    def test_login_page(self):
+        pong = self.get_ping().get('/', content_type="html/text")
+        self.assertIn(b'Yummy Recipes - Login', pong.data)
+
+    def test_index_page(self):
+        pong = self.get_ping().get('/home', content_type="html/text")
+        self.assertIn(b'Yummy Recipes - Home', pong.data)
+
+    def test_dashboard_page(self):
+        pong = self.get_ping().get('/dashboard', content_type="html/text")
+        self.assertIn(b'Yummy Recipes - Dashboard', pong.data)
+
 
 if __name__ == "__main__":
     unittest.main()
